f5a065014457f505390dd60de7137fa60b8802b8
Jesper Kristensen <cableman@linuxdev.dk>
2011-10-27 21:13:40 +0200

    Added comment to swtor account module

--- sites/all/modules/swtor_account/templates/swtor-account.tpl.php
+++ sites/all/modules/swtor_account/templates/swtor-account.tpl.php
@@ -1,6 +1,21 @@
+<?php
+/**
+ * @file swtor-account.tpl.php
+ * Default theme implementation for swtor-account block
+ *
+ * Available variables:
+ * - $links: The linkst listed in under account infomation.
+ * - $links_raw: List of links in array, before theme('links', $links).
+ * - $messages: The number of unread message.
+ * - $links_message: Message links in array, before theme('links', $messages).
+ *
+ */
+?>
 <div id="swtor-account">
   <h2><?php print t('Welcome') . ' ' . $username ?></h2>
-  <?php print $links ?>
-  <?php print $messages ?>
-  <a href="/logout">Logout</a>
+  <div class="content">
+    <?php print $links ?>
+    <?php print $messages ?>
+    <a href="/logout">Logout</a>
+  </div>
 </div>
